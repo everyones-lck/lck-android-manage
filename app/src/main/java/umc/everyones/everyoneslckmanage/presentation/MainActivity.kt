f2b8731d9ba2b942cea20fc2d8f9cd29df889ffd
KKJ <kyujin0911@tukorea.ac.kr>
2024-09-23 23:53:31 +0900

    [UI/#3] 홈화면 UI 구현

--- app/src/main/java/umc/everyones/everyoneslckmanage/presentation/MainActivity.kt
+++ app/src/main/java/umc/everyones/everyoneslckmanage/presentation/MainActivity.kt
@@ -5,19 +5,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 import umc.everyones.everyoneslckmanage.R
+import umc.everyones.everyoneslckmanage.databinding.ActivityMainBinding
+import umc.everyones.everyoneslckmanage.presentation.base.BaseActivity
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private lateinit var navController: NavController
+    override fun initView() {
+        initNavigator()
+    }
+
+    override fun initObserver() {
+    }
+
+    private fun initNavigator() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
---
+++ app/src/main/java/umc/everyones/everyoneslckmanage/presentation/home/HomeFragment.kt
@@ -0,0 +1,15 @@
+package umc.everyones.everyoneslckmanage.presentation.home
+
+import umc.everyones.everyoneslckmanage.R
+import umc.everyones.everyoneslckmanage.databinding.FragmentHomeBinding
+import umc.everyones.everyoneslckmanage.presentation.base.BaseFragment
+import java.time.LocalDate
+
+class HomeFragment: BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    override fun initObserver() {
+
+    }
+
+    override fun initView() {
+    }
+}
