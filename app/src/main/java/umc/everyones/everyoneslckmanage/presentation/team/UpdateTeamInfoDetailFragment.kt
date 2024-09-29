6018367708e268195aa46b09ba826751a91ef3e7
hyowon0204 <ally0204@naver.com>
2024-09-30 04:42:13 +0900

    [UI/#9] 팀 정보 입력, detail 정보 입력 ui 구현

---
+++ app/src/main/java/umc/everyones/everyoneslckmanage/presentation/team/UpdateTeamInfoDetailFragment.kt
@@ -0,0 +1,78 @@
+package umc.everyones.everyoneslckmanage.presentation.team
+
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import umc.everyones.everyoneslckmanage.R
+import umc.everyones.everyoneslckmanage.databinding.FragmentUpdateTeamInfoDetailBinding
+import umc.everyones.everyoneslckmanage.presentation.base.BaseFragment
+import umc.everyones.everyoneslckmanage.util.extension.setOnSingleClickListener
+
+class UpdateTeamInfoDetailFragment : BaseFragment<FragmentUpdateTeamInfoDetailBinding>(R.layout.fragment_update_team_info_detail) {
+
+    private val navigator by lazy {
+        findNavController()
+    }
+
+    override fun initObserver() {
+    }
+
+    override fun initView() {
+        setupBackButtonListener()
+        setupTeamName()
+        setupLckRoasterNavigation()
+        setupLckClRoasterNavigation()
+        setupLckCoachNavigation()
+    }
+
+    private fun setupBackButtonListener() {
+        binding.ivUpdateTeamDetailPrevious.setOnClickListener {
+            val action = UpdateTeamInfoDetailFragmentDirections
+                .actionUpdateTeamInfoDetailToUpdateTeamInfoFragment()
+            navigator.navigate(action)
+        }
+    }
+
+    private fun setupTeamName() {
+        val teamName = arguments?.getString("teamName") ?: "Unknown Team"
+        binding.tvUpdateTeamDetailTeamName.text = teamName
+    }
+
+    private fun setupLckRoasterNavigation() {
+        binding.tvUpdateTeamDetailLckRoaster.setOnClickListener {
+            val teamName = binding.tvUpdateTeamDetailTeamName.text.toString()
+            val action = UpdateTeamInfoDetailFragmentDirections
+                .actionUpdateTeamInfoDetailToUpdateTeamInfoLckRoasterFragment(
+                    newPlayer = null,
+                    updatedRoaster = null,
+                    teamName = teamName
+                )
+            navigator.navigate(action)
+        }
+    }
+
+    private fun setupLckClRoasterNavigation() {
+        binding.tvUpdateTeamDetailLckClRoaster.setOnClickListener {
+            val teamName = binding.tvUpdateTeamDetailTeamName.text.toString()
+            val action = UpdateTeamInfoDetailFragmentDirections
+                .actionUpdateTeamInfoDetailToUpdateTeamInfoLckClRoasterFragment(
+                    newPlayer = null,
+                    updatedRoaster = null,
+                    teamName = teamName
+                )
+            navigator.navigate(action)
+        }
+    }
+
+    private fun setupLckCoachNavigation() {
+        binding.tvUpdateTeamDetailLckCoach.setOnClickListener {
+            val teamName = binding.tvUpdateTeamDetailTeamName.text.toString()
+            val action = UpdateTeamInfoDetailFragmentDirections
+                .actionUpdateTeamInfoDetailToUpdateTeamInfoLckCoachFragment(
+                    newPlayer = null,
+                    updatedRoaster = null,
+                    teamName = teamName
+                )
+            navigator.navigate(action)
+        }
+    }
+}
--- app/src/main/java/umc/everyones/everyoneslckmanage/presentation/team/UpdateTeamInfoFragment.kt
+++ app/src/main/java/umc/everyones/everyoneslckmanage/presentation/team/UpdateTeamInfoFragment.kt
@@ -1,16 +1,54 @@
 package umc.everyones.everyoneslckmanage.presentation.team
 
+
+import androidx.navigation.fragment.findNavController
 import umc.everyones.everyoneslckmanage.R
-import umc.everyones.everyoneslckmanage.databinding.FragmentInputMatchInfoBinding
 import umc.everyones.everyoneslckmanage.databinding.FragmentUpdateTeamInfoBinding
 import umc.everyones.everyoneslckmanage.presentation.base.BaseFragment
+import umc.everyones.everyoneslckmanage.util.extension.setOnSingleClickListener
 
 class UpdateTeamInfoFragment : BaseFragment<FragmentUpdateTeamInfoBinding>(R.layout.fragment_update_team_info) {
+
+    private val navigator by lazy {
+        findNavController()
+    }
     override fun initObserver() {
 
     }
 
     override fun initView() {
+        setTeamNameClickListener()
+        setupBackButtonListener()
+    }
 
+    private fun setTeamNameClickListener() {
+        val teamTextViews = listOf(
+            binding.tvUpdateTeamGen,
+            binding.tvUpdateTeamT1,
+            binding.tvUpdateTeamHle,
+            binding.tvUpdateTeamDk,
+            binding.tvUpdateTeamKt,
+            binding.tvUpdateTeamDrx,
+            binding.tvUpdateTeamNs,
+            binding.tvUpdateTeamBro,
+            binding.tvUpdateTeamKdf,
+            binding.tvUpdateTeamBnk
+        )
+
+        teamTextViews.forEach { textView ->
+            textView.setOnSingleClickListener {
+                val teamName = textView.text.toString()
+                val action = UpdateTeamInfoFragmentDirections.actionUpdateTeamInfoFragmentToUpdateTeamInfoDetailFragment(teamName)
+                navigator.navigate(action)
+            }
+        }
+    }
+
+    private fun setupBackButtonListener() {
+        binding.ivUpdateTeamPrevious.setOnSingleClickListener  {
+            val action = UpdateTeamInfoFragmentDirections
+                .actionUpdateTeamInfoFragmentToHomeFragment()
+            navigator.navigate(action)
+        }
     }
 }
